a0f478570f45407b56eb95366b64c9f7f7c5de12
FrancescoGuggino <landongu@gmail.com>
2023-09-18 15:52:31 +0200

    add show method

--- src/main/java/org/learning/pizzeria/spring/la/mia/pizzeria/crud/controller/PizzeriaController.java
+++ src/main/java/org/learning/pizzeria/spring/la/mia/pizzeria/crud/controller/PizzeriaController.java
@@ -3,12 +3,16 @@ package org.learning.pizzeria.spring.la.mia.pizzeria.crud.controller;
 import org.learning.pizzeria.spring.la.mia.pizzeria.crud.model.Pizza;
 import org.learning.pizzeria.spring.la.mia.pizzeria.crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -24,6 +28,19 @@ public class PizzeriaController {
         model.addAttribute("pizze",pizzaList);
 
         return "pizze";
+    }
+
+    @GetMapping("/show/{pizzaId}")
+    public String show (@PathVariable("pizzaId") Integer id, Model model){
+        Optional<Pizza> pizzaOptional = pizzaRepository.findById(id);
+
+        if (pizzaOptional.isPresent()){
+        Pizza pizzaFromDB = pizzaOptional.get();
+        model.addAttribute("pizza",pizzaFromDB);
+        return "/detail";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizze (name,price,description) VALUES ('Margherita', '5€', 'Info')
+INSERT INTO pizze (name,price,description) VALUES ('Siciliana', '5€', 'Info')
+INSERT INTO pizze (name,price,description) VALUES ('Marinara', '5€', 'Info')
